ed172e22132ca9d204bb51d8c6704f64a82d0f1d
MichaelOliviero <31869511+MichaelOliviero@users.noreply.github.com>
2018-10-07 17:07:55 -0400

    Class: Tile

--- Rummikub-COMP3004/src/main/java/Tile.java
+++ Rummikub-COMP3004/src/main/java/Tile.java
@@ -1,4 +1,25 @@
-
 public class Tile {
+	// Variables
+	private int colour;
+	private int value;
+	private static final String[] colours = { "Red", "Green", "Black", "Orange" };
+	
+	// Constructor for string & int
+	public Tile (String colour, int value) {
+		// Make the string uppercase
+		colour = colour.toUpperCase();
+		
+		// Set the colour of the tile
+		if (colour.equals("R")) { this.colour = 0;}
+		else if (colour.equals("G")) { this.colour = 1; }
+		else if (colour.equals("B")) { this.colour = 2; }
+		else if (colour.equals("O")) { this.colour = 3; }
+		
+		// Set the value of the tile
+		this.value = value;
+	}
+	
+	public String getColour() { return colours[this.colour]; } // Getter for tile colour
+	public int getValue() { return this.value; }	// Getter for tile value
 
 }
