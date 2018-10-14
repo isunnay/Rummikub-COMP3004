f8e688fcfd4e3e64d60157ac5bb4ef02ed7772d6
Joe <joe.santhosh88@yahoo.com>
2018-10-14 10:24:47 -0400

    Refactor: HandTest.java - made some changes to test cases: using deck with hand

--- Rummikub-COMP3004/src/test/java/HandTest.java
+++ Rummikub-COMP3004/src/test/java/HandTest.java
@@ -3,16 +3,28 @@ import junit.framework.TestCase;
 public class HandTest extends TestCase {
 	public void testHandExists() {
 		Hand hand = new Hand();
+		Deck deck = new Deck();
+		
+		hand.createHandFromDeck(deck);
+		
 		assertNotNull(hand);
 	}
 	
 	public void testNumTiles() {
 		Hand hand = new Hand();
+		Deck deck = new Deck();
+		
+		hand.createHandFromDeck(deck);
+		
 		assertEquals(14, hand.getNumTiles());
 	}
 	
 	public void testMeld() {
 		Hand hand = new Hand();
+		Deck deck = new Deck();
+		
+		hand.createHandFromDeck(deck);
+		
 		assertTrue("true", hand.meldExists());
 	}
 }
