824b459e12c1b8388ac9a829962abfbb0191b1e9
Elvin Sunny <elvin.sunny.07@gmail.com>
2018-10-20 06:26:18 -0400

    Interface: Subject.java & Observer.java - Going to be used to implement the Observer pattern

---
+++ Rummikub-COMP3004/src/main/java/Observer.java
@@ -0,0 +1,6 @@
+
+public interface Observer {
+	
+	public void update(int x, int y);
+
+}
---
+++ Rummikub-COMP3004/src/main/java/Subject.java
@@ -0,0 +1,10 @@
+
+public interface Subject {
+
+	public void registerObserver(Observer o);
+		
+	public void removeObserver(Observer o);
+		
+	public void notifyObservers();
+
+}
