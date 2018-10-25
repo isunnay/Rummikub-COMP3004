b9776487d6fc83e01c17f85ac655de8ae753bc54
Elvin Sunny <elvin.sunny.07@gmail.com>
2018-10-24 20:53:14 -0400

    Refactor: BoardTest.java - Added testCases as well as modified old testCases

--- Rummikub-COMP3004/src/test/java/BoardTest.java
+++ Rummikub-COMP3004/src/test/java/BoardTest.java
@@ -1,33 +1,42 @@
+import junit.framework.TestCase;
 
-public class BoardTest {
-	public void testDeckExists() {
+public class BoardTest extends TestCase {
+	
+	public void testBoardExists() {
 		Board board = new Board();
 		assertNotNull(board);	
 	}
 	
-	public void testAddTile() {
+	public void testPlayTile() {
 		Board board = new Board();
 		Deck deck = new Deck();
 		Hand playerHand = new Hand();
 		playerHand.createHand(deck);
 		String colour = playerHand.getTile(1).getColour();
 		int value = playerHand.getTile(1).getValue();
-		board.add(playerHand.getTile(colour, value));
+		board.playTile(playerHand.getTile(colour, value), x, y);
 		
 		assertEquals(1, getNumberOfBoardTiles());		
 	} 
 	
-	public void testGetBoardSize() {
+	public void testGetBoardNumberOfTiles() {
 		Board board = new Board();
 		Deck deck = new Deck();
 		Hand playerHand = new Hand();
 		playerHand.createHand(deck);
 		String colour = playerHand.getTile(1).getColour();
 		int value = playerHand.getTile(1).getValue();
-		board.add(playerHand.getTile(colour, value), 1, 1);
+		board.playTile(playerHand.getTile(colour, value), 1, 1);
 		
 		assertEquals(1, getNumberOfBoardTiles());		
+	}
 	
+	public void testBoardSize() {
+		Board board = new Board();
+		int x = board.getX();
+		int y = board.getY();
+		
+		assertEquals(15, x+y);
 	}
 	
 	public void testIsLocationFilled() {
@@ -37,9 +46,9 @@ public class BoardTest {
 		hand.createHand(deck);
 		int xLocation = 1;
 		int yLocation = 1;		
-		board.add(hand.getTile(1), xLocation, yLocation);
+		board.playTile(hand.getTile(1), xLocation, yLocation);
 
-		assertTue("true", isLocationFilled(xLocation, yLocation));		
+		assertTue("true", isSpotFilled(xLocation, yLocation));		
 	}
 	
 	public void testMoveTiles() {
@@ -49,12 +58,60 @@ public class BoardTest {
 		hand.createHand(deck);
 		int xLocation = 1;
 		int yLocation = 1;		
-		board.add(hand.getTile(1), xLocation, yLocation);
+		board.playTile(hand.getTile(1), xLocation, yLocation);
 		int newX = 2;
 		int newY = 2;
-		board.move(board.getTile(xLocation,yLocation), newX, newY);
+		board.moveTile(board.getTileAtSpot(xLocation,yLocation), newX, newY);
+		assertTrue("true", hasTileMoved(board.getTile(xLocation,yLocation)));	
+	}
+	
+	public void testIsValidMeld() {
+		Board board = new Board();
+		Deck deck = new Deck();
+		Hand hand = new Hand();
+		hand.createHand(deck);
+		//boolean isValidMeld = false;
+		board.playTile(hand.getTile(0), 0, 0);
+		board.playTile(hand.getTile(1), 1, 0);
+		board.playTile(hand.getTile(2), 2, 0);
 		
-		assertTrue("true", hasTileMoved(board.getTile(xLocation,yLocation)));
+		assertTrue("true", board.isValidMeld(2));	
+	}
+	
+	public void testIsBoardValidTrue() {
+		Board board = new Board();
+		Deck deck = new Deck();
+		Hand hand = new Hand();
+		hand.createHand(deck);
+		board.playTile(hand.getTile(0), 0, 0);
+		board.playTile(hand.getTile(1), 1, 0);
+		board.playTile(hand.getTile(2), 2, 0);
 		
+		assertTrue("true",board.isBoardValid());		
+	}
+	
+	public void testIsBoardValidFalse() {
+		Board board = new Board();
+		Deck deck = new Deck();
+		Hand hand = new Hand();
+		hand.createHand(deck);
+		board.playTile(hand.getTile(0), 0, 1);
+		board.playTile(hand.getTile(1), 1, 10);
+		board.playTile(hand.getTile(2), 5, 6);
+		
+		assertFalse("false",board.isBoardValid());		
+	}
+	
+	public void testRemoveTile() {
+		Board board = new Board();
+		Deck deck = new Deck();
+		Hand playerHand = new Hand();
+		playerHand.createHand(deck);
+		String colour = playerHand.getTile(1).getColour();
+		int value = playerHand.getTile(1).getValue();
+		int x = 5; int y = 5;
+		board.playTile(playerHand.getTile(colour, value), x, y);
+		
+		board.removeTile(x,y);
 	}
 }
