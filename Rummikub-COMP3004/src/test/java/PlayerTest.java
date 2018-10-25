2ef2490a350261eb4f476c67f687a4dc0c9cb0a8
roshan <rshn.mathew@gmail.com>
2018-10-25 16:23:33 -0400

    Refactor: PlayerTest.java - added test cases

--- Rummikub-COMP3004/src/test/java/PlayerTest.java
+++ Rummikub-COMP3004/src/test/java/PlayerTest.java
@@ -1,5 +1,30 @@
 import junit.framework.TestCase;
  
 public class PlayerTest extends TestCase{
+	
+public void  testTurn() { 
+		
+		Player player = new Player ();
+		player.setIsItPlayersTurn(true); 
+		assertTrue(player.getIsItPlayersTurn()); 
+		player.setIsItPlayersTurn(false); 
+		assertFalse(player.getIsItPlayersTurn()); 
+	}
+	
+	public void testHasTileBeenTaken() { 
+		
+		Player player = new Player ();
+		assertFalse(player.getHasTileBeenTaken()); 
+		player.setHasTileBeenTaken(true); 
+		assertTrue(player.getHasTileBeenTaken()); 
+	}
+	
+	public void testHasTileBeenPlaced() { 
+		
+		Player player = new Player ();
+		assertFalse(player.getHasTileBeenPlaced()); 
+		player.setHasTileBeenPlaced(true); 
+		assertTrue(player.getHasTileBeenPlaced()); 
+	}
 
 }
