712775f5caa5ceef715a4a9b8ae8611b2b79a898
Joe <joe.santhosh88@yahoo.com>
2018-10-25 16:04:23 -0400

    Refactor: FileInputTest.java - added some test cases

--- Rummikub-COMP3004/src/test/java/FileInputTest.java
+++ Rummikub-COMP3004/src/test/java/FileInputTest.java
@@ -1,5 +1,43 @@
 import junit.framework.TestCase;
 
 public class FileInputTest extends TestCase {
+	public void testFile() {
+		Game game = new Game();
+		
+		assertNotNull(game.chooseFile());
+	}
 	
+	public void testAI1hasInitialMeld() {
+		Game game = new Game();
+		
+		game.chooseFile();
+		Tile tile = new Tile("R", 4);
+		Tile tile2 = new Tile("R", 5);
+		Tile tile3 = new Tile("R", 6);
+		Tile tile4 = new Tile("R", 7);
+		Tile tile5 = new Tile("R", 8);
+		
+		Tile[] tiles = {tile, tile2, tile3, tile4, tile5};
+		
+		Deck deck = new Deck();
+		
+		for(int i=0; i<tiles.length; i++) {
+			game.getPlayer(1).getPlayerHand().add(tiles[i]);
+			deck.getTileDeck().remove(tiles[i]);
+		}
+		
+		for(int i=0; i<9; i++) {
+			game.getPlayer(1).getPlayerHand().add(deck.getTileDeck().remove(deck.getTileDeck().size() - 1));
+		}
+		
+		assertTrue("true", game.hasInitialMeld(game.getPlayer(1)));
+	}
+	
+	public void testTilesPlayed() {
+		Game game = new Game();
+		
+		game.chooseFile();
+		game.getPlayer(1).playTile();
+		assertTrue("true", game.tilesPlayed(game.getPlayer(1)));
+	}
 }
