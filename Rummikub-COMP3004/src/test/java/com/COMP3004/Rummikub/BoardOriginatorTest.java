eaddda566edcd8d2700a152015fcf9d76070dad1
Joe <joe.santhosh88@yahoo.com>
2018-12-04 17:56:44 -0500

    Test: BoardOriginatorTest.java - test for originator existing

--- Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardOriginatorTest.java
+++ Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardOriginatorTest.java
@@ -3,9 +3,9 @@ package com.COMP3004.Rummikub;
 import junit.framework.TestCase;
 
 public class BoardOriginatorTest extends TestCase {
-	public void testOriginatorExists() {
+	public void testBoardOriginator() {
 		BoardOriginator originator = new BoardOriginator();
-		
+	
 		assertNotNull(originator);
 	}
 }
