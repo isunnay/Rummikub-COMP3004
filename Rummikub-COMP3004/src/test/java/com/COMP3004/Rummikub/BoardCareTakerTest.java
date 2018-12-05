415e06edcf87ea164cf4b223cb5eea980776c9d5
Joe <joe.santhosh88@yahoo.com>
2018-12-04 19:45:34 -0500

    Refactor: BoardCareTakerTest and BoardOriginatorTest - added more test cases

--- Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardCareTakerTest.java
+++ Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardCareTakerTest.java
@@ -1,12 +1,25 @@
 package com.COMP3004.Rummikub;
 
 import junit.framework.TestCase;
+
+import com.COMP3004.Rummikub.models.Board;
 import com.COMP3004.Rummikub.models.BoardCareTaker;
+import com.COMP3004.Rummikub.models.BoardMomento;
 
 public class BoardCareTakerTest extends TestCase {
 	public void testBoardCareTakerExists() {
 		BoardCareTaker careTaker = new BoardCareTaker();
 		
 		assertNotNull(careTaker);
+	}
+	
+	public void testMementoAdded() {
+		Board board = new Board();
+		BoardMomento memento = new BoardMomento(board);
+		
+		BoardCareTaker careTaker = new BoardCareTaker();
+		careTaker.addMomento(memento);
+		
+		assertTrue(careTaker.momentoExists(memento));
 	}
 }
--- Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardOriginatorTest.java
+++ Rummikub-COMP3004/src/test/java/com/COMP3004/Rummikub/BoardOriginatorTest.java
@@ -1,12 +1,38 @@
 package com.COMP3004.Rummikub;
 
 import junit.framework.TestCase;
+
+import com.COMP3004.Rummikub.models.Board;
+import com.COMP3004.Rummikub.models.BoardMomento;
 import com.COMP3004.Rummikub.models.BoardOriginator;;
 
 public class BoardOriginatorTest extends TestCase {
 	public void testBoardOriginator() {
-		BoardOriginator originator = new BoardOriginator();
+		Board board = new Board();
+		BoardOriginator originator = new BoardOriginator(board);
 	
 		assertNotNull(originator);
+	}
+	
+	public void testStateSavedToMomento() {
+		Board board = new Board();
+		BoardOriginator originator = new BoardOriginator(board);
+		
+		BoardMomento momento = new BoardMomento(baord);
+		
+		assertEquals(momento, originator.saveStateToMomento());
+	}
+	
+	public void testGetStateFromMomento() {
+		Board board = new Board();
+		BoardOriginator originator = new BoardOriginator(board);
+		
+		Board board2 = new Board();
+		
+		BoardMomento momento = new BoardMomento(board2);
+		
+		originator.getStateFromMomento(momento);
+		
+		assertEquals(board2, originator.getState());
 	}
 }
