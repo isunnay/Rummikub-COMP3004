5f9bf13f10bfc01196858fe8705ad15449980dcc
Joe <joe.santhosh88@yahoo.com>
2018-12-04 20:11:18 -0500

    Added functionality in BoardCareTaker.java and BoardOriginator.java to check whether test cases will pass

--- Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardCareTaker.java
+++ Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardCareTaker.java
@@ -1,7 +1,19 @@
 package com.COMP3004.Rummikub.models;
 
+import java.util.ArrayList;
+
 public class BoardCareTaker {
-	public void BoardCareTaker() {
-		
+	private ArrayList<BoardMomento> boardMomentos = new ArrayList<BoardMomento>();
+	
+	public void addMomento(BoardMomento momento) {
+		boardMomentos.add(momento);
+	}
+	
+	public BoardMomento getMomento(int i) {
+		return boardMomentos.get(i);
+	}
+	
+	public boolean momentoExists(BoardMomento momento) {
+		return boardMomentos.contains(momento);
 	}
 }
--- Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardOriginator.java
+++ Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardOriginator.java
@@ -1,7 +1,19 @@
 package com.COMP3004.Rummikub.models;
 
 public class BoardOriginator {
-	public BoardOriginator() {
-		
+	private Board boardState;
+	
+	public BoardOriginator(Board board) {
+		boardState = board;
+	}
+	
+	public Board getState() { return boardState; }
+	
+	public BoardMomento saveStateToMomento() {
+		return new BoardMomento(boardState);
+	}
+	
+	public void getStateFromMomento(BoardMomento momento) {
+		boardState = momento.getBoardState();
 	}
 }
