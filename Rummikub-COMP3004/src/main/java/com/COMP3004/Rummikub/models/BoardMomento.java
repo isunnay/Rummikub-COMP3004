dbd14d32b30aeb287fb3c6bd824048f6f8625067
Joe <joe.santhosh88@yahoo.com>
2018-12-04 19:44:16 -0500

    Refactor: BoardMomento.java - added getBoardState() method and changed constructor

--- Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardMomento.java
+++ Rummikub-COMP3004/src/main/java/com/COMP3004/Rummikub/models/BoardMomento.java
@@ -1,7 +1,11 @@
 package com.COMP3004.Rummikub.models;
 
 public class BoardMomento {
-	public BoardMomento() {
-		
+	private Board boardState;
+	
+	public BoardMomento(Board board) {
+		boardState = board;
 	}
+	
+	public Board getBoardState() { return boardState; }
 }
